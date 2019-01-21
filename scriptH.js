4cd4802a91dca12bc68190ca1f2a64f5ad2f64ff
akshay-4079 <46236877+akshay-4079@users.noreply.github.com>
2019-01-21 03:02:12 -0600

    Final Changes

--- scriptH.js
+++ scriptH.js
@@ -200,3 +200,14 @@ function MinesweeperController($scope) {
         }
     };
 }
+function Changebck(a){
+    console.log(a);
+    if (a=='i1')
+    {document.getElementById('area').style.backgroundImage="url(https://www.pixelstalk.net/wp-content/uploads/2016/07/8-Bit-Backgrounds.jpg)";}
+     if (a=='i2')
+    {document.getElementById('area').style.backgroundImage="url(https://d2v9y0dukr6mq2.cloudfront.net/video/thumbnail/dHnxL5V/retro-video-game-3d-landscape-computer-vector-8-bit-arcade-flyover-wireframe-terrain-4k_nwly8eeae__F0000.png)";}
+     if (a=='i3')
+    {document.getElementById('area').style.backgroundImage="url(https://media.giphy.com/media/l0HlM6oMVfvqZgSMU/giphy.gif)";}
+     if (a=='i4')
+    {document.getElementById('area').style.backgroundImage="url(https://d2v9y0dukr6mq2.cloudfront.net/video/thumbnail/geometric-8-bit-game_wjbjcv-r__F0000.png)";}
+}
